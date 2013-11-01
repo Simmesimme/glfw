b3579c67a888f76a438b3919f51c7215047761cd
Camilla Berglund <elmindreda@elmindreda.org>
2013-11-01 13:55:17 +0100

    Simplified X11 monitor setting.

--- src/x11_window.c
+++ src/x11_window.c
@@ -1130,16 +1130,12 @@ void _glfwPlatformSetWindowMonitor(_GLFWwindow* window,
                                    int width, int height)
 {
     if (window->monitor)
-    {
         leaveFullscreenMode(window);
-        _glfwInputWindowMonitorChange(window, NULL);
-    }
 
-    if (monitor)
-    {
-        _glfwInputWindowMonitorChange(window, monitor);
+    _glfwInputWindowMonitorChange(window, monitor);
+
+    if (window->monitor)
         enterFullscreenMode(window);
-    }
 }
 
 void _glfwPlatformPollEvents(void)
